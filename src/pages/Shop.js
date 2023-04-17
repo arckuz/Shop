ada0808ecf5713834abc835be2d9c4367cf6d812
arckuz <arckuz@mail.ru>
2023-04-17 22:27:09 +0700

    главная страница магазина

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {Col, Container, Row} from "react-bootstrap";
+import TypeBar from "../components/TypeBar";
+import BrandBar from "../components/BrandBar";
+import DeviceList from "../components/DeviceList";
 
 const Shop = () => {
     return (
-        <div>
-            Shop
-        </div>
+        <Container className="mt-2">
+            <Row >
+                <Col md ={3}>
+                    <TypeBar></TypeBar>
+                </Col>
+
+                <Col md ={9} >
+                    <BrandBar></BrandBar>
+                    <DeviceList ></DeviceList>
+
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
