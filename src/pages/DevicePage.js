2cfd187d22e6fe339238035fd542954c78a9a327
arckuz <arckuz@mail.ru>
2023-04-17 22:25:48 +0700

    траница товара

--- src/pages/DevicePage.js
+++ src/pages/DevicePage.js
@@ -1,10 +1,48 @@
 import React from 'react';
-
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
+import bigStar from '../assets/bigStar.png'
 const DevicePage = () => {
+    const  device = {id: 8, name: "желтая футболка", price: "2500", img: 'хз'}
+    const description=[
+        {id:1, tytle:"материал",description:"хлопок"},
+        {id:2, tytle:"материал",description:"хлопок"},
+        {id:3, tytle:"материал",description:"хлопок"},
+        {id:4, tytle:"материал",description:"хлопок"},
+    ]
     return (
-        <div>
-            DevicePage
-        </div>
+        <Container className="mt-3">
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={device.img}/>
+                </Col>
+                <Col md={4}>
+                    <Row className="d-flex flex-column align-items-center">
+                        <h2>{device.name}</h2>
+                    </Row>
+                    <div
+                        className="d-flex justify-content-center align-items-center"
+                        style={{background: `url(${bigStar}) no-repeat center center`, width:240, height: 240, backgroundSize: 'cover', fontSize:64}}
+                    ></div>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justify-content-around"
+                        style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
+                    >
+                        <h3>От: {device.price} руб.</h3>
+                        <Button variant={"outline-dark"}>Добавить в корзину</Button>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="d-flex flex-column m-3">
+                <h1>Характеристики</h1>
+                {description.map((info, index) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                        {info.tytle}: {info.description}
+                    </Row>
+                )}
+            </Row>
+        </Container>
     );
 };
 
--- src/store/DeviceStore.js
+++ src/store/DeviceStore.js
@@ -4,19 +4,32 @@ export default class DeviceStore{
     constructor() {
         this._types = [
             {id: 1, name: "Футболки"},
-            {id: 2, name: "Наклейки"}
+            {id: 2, name: "Наклейки"},
+            {id: 3, name: "Кепки"},
+            {id: 4, name: "Картхолдеры"},
+            {id: 5, name: "Кофты"},
+            {id: 6, name: "Браслеты"}
         ]
         this._brands = [
-            {id: 1, name: "Футболки"},
-            {id: 2, name: "Наклейки"}
+            {id: 1, name: "Гучи"},
+            {id: 2, name: "Прада"},
+            {id: 3, name: "Гучи"},
+            {id: 4, name: "Прада"},
+
         ]
         this._device = [
             {id: 1, name: "желтая футболка", price: "2500", img: 'хз'},
             {id: 2, name: "желтая футболка", price: "2500", img: 'хз'},
             {id: 3, name: "желтая футболка", price: "2500", img: 'хз'},
             {id: 4, name: "желтая футболка", price: "2500", img: 'хз'},
+            {id: 5, name: "желтая футболка", price: "2500", img: 'хз'},
+            {id: 6, name: "желтая футболка", price: "2500", img: 'хз'},
+            {id: 7, name: "желтая футболка", price: "2500", img: 'хз'},
+            {id: 8, name: "желтая футболка", price: "2500", img: 'хз'},
 
         ]
+        this._selectedType = {}
+        this._selectedBrand = {}
         makeAutoObservable(this)
     }
 
@@ -29,13 +42,26 @@ export default class DeviceStore{
     setDevice(device){
         this._device = device
     }
+
+    setSelectedType(type){
+        this._selectedType=type;
+    }
+    setSelectedBrand(brand){
+        this._selectedBrand=brand;
+    }
     get types(){
         return this._types
     }
     get brands(){
         return this._brands
     }
-    get device(){
+    get devices(){
         return this._device
+    }
+    get selectedType(){
+        return this._selectedType
+    }
+    get selectedBrand(){
+        return this._selectedBrand
     }
 }
